17e865d84637d3a87ccf497ab0613a0cc90f3e2c
MonkeyChuang <j28823631@gmail.com>
2016-04-05 14:46:06 +0800

    files for lab3.

---
+++ lab3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> Weights;
+    int weight,sum=0;
+    int num_cow;
+    ifstream infile("file.in",ios::in);
+    if(!infile)
+    {
+        cout<<"Failed to open file.in"<<endl;
+        exit(1);
+    }
+    infile>>num_cow;
+    if(num_cow<5)
+    {
+        cout<<"Too few cows."<<endl;
+        exit(1);
+    }
+    while(infile>>weight)
+        Weights.push_back(weight);
+    sort(Weights.begin(),Weights.end());
+    for(int i=num_cow-5;i<num_cow;i++)
+        sum+=Weights.at(i);
+    cout<<sum<<endl;
+
+    return 0;
+}
